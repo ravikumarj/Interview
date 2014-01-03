f0cf5452fa139279496d9e826191d3340d2cc9d0
ravikumar <ravijey.psgit@gmail.com>
2014-01-02 20:14:56 -0800

    Has Path

--- bst.cpp
+++ bst.cpp
@@ -97,17 +97,62 @@ int maxDepth(struct node *root)
 
 }
 
+int minValue(struct node *root)
+{
+	if(root->left == NULL)
+		return root->data;
+	else
+	{
+		int min=minValue(root->left);
+		return min;
+	}
 
 
+}
 
+void print(struct node *root)
+{
+if(root == NULL)
+	return;
+print(root->left);
+cout<<"\n"<<root->data;
+print(root->right);
+}
+
+bool hasPath(struct node *root,int sum)
+{
+	i//Empty tree
+	if(root == NULL)
+	{
+		return(sum == 0);
+	}
+	else
+	{
+	bool left=false;
+	bool right=false;	
+	int sub=sum-root->data;
+	if(sub == 0 && root->left == NULL && root->right == NULL)
+		return true;
+	if(root->left)
+	 left=hasPath(root->left,sub);
+	if(root->right)
+	 right=hasPath(root->right,sub);
+
+	return (left||right);
+
+	}
+}
 main()
 {
 	struct node *tree=NULL;
 	tree=insert(tree,5);
 	tree=insert(tree,4);
 	tree=insert(tree,3);
-	tree=insert(tree,8);
+
+/*	tree=insert(tree,8);
 	tree=insert(tree,6);
+	tree=insert(tree,2);
+	tree=insert(tree,1);*/
 
 	cout<<lookup(tree,5);
 	cout<<"\n"<<lookup(tree,3);
@@ -116,4 +161,12 @@ main()
 	cout<<"\nnumber of nodes "<<c<<endl;
 	int height=maxDepth(tree);
 	cout<<"\nDepth -->"<<height;
+	int min=minValue(tree);
+	cout<<"\nMin -->"<<min;
+
+	cout<<"\nPrint tree \n";
+	print(tree);
+	cout<<"\nHas Sum\n";
+	cout<<hasPath(tree,12)<<endl;;
+	cout<<hasPath(tree,9);
 }
